dc59ab00126fa1901e7e516aa637b74e2a1e4b31
Diean233 <wdxiop@outlook.com>
2024-02-25 22:53:46 -0800

    favorite & food list

--- src/views/restaurantcard.js
+++ src/views/restaurantcard.js
@@ -1,17 +1,36 @@
-import { MdOutlineFoodBank } from "react-icons/md";
+import React, { useState } from 'react';
+import { MdOutlineFoodBank, MdOutlineStarPurple500, MdOutlineStarOutline } from "react-icons/md";
 
 import { Link } from 'react-router-dom';
 
 export default function RestaurantCard({ restaurant }) {
     const { id, name, foods, hours, wait_time, location } = restaurant;
+    // foods
+
+    const [isCollapsed, setIsCollapsed] = useState(true); // State to manage collapsible component visibility
+
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed); // Toggle the state when clicking on the main component
+    };
 
     return (
-        <div className="item">
-            <MdOutlineFoodBank />
-            <div className="content">
-                <div>{name}</div>
-                <div>{"wait time: " + wait_time}</div>
+        <div>
+            <div className="card"  onClick={toggleCollapse}>
+                {/* {<MdOutlineFoodBank />} */}
+                <div className="content">
+                    <div>{name}</div>
+                    <div>{"wait time: " + wait_time}</div>
+                </div>
+                <div className="fav">
+                    <MdOutlineStarOutline />
+                </div>
             </div>
+            {!isCollapsed && (
+            <div className="collapsible">
+            {/* Collapsible content */}
+            Here comes the food list...
+            </div>
+            )}
         </div>
     );
 }
