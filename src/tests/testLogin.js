40d01bf8fc5807f06d68b413363ddbf956159658
Hitvarth Diwanji <hitvarthdiwanji@Hitvarths-MacBook-Pro.local>
2024-03-04 13:45:09 -0800

    added some more tests

--- src/tests/testLogin.js
+++ src/tests/testLogin.js
@@ -12,7 +12,8 @@ async function loginTest() {
     await driver.findElement(By.name("password")).sendKeys("Password@12345");
     // Select login button and invoke click action
     //If login details are correct we wiil be redirected to the welcome page
-    await driver.findElement(By.className("ui button green")).click(); 
+    await driver.findElement(By.className("ui button green")).click();
+    await driver.sleep(2000);
     var pageUrl = await driver.getCurrentUrl();
     // the user should stay on the login page
     assert.strictEqual(pageUrl, "http://localhost:3000/auth");
@@ -23,9 +24,36 @@ async function loginTest() {
     // Select input elements and fill them out
     await driver.findElement(By.name("email")).sendKeys("test3@gmail.com");
     await driver.findElement(By.name("password")).sendKeys("Password@12345");
-    // Select login button and invoke click action
-    //If login details are correct we wiil be redirected to the welcome page
     await driver.findElement(By.className("ui button green")).click(); 
+    await driver.sleep(2000);
+    var pageUrl = await driver.getCurrentUrl();
+    assert.strictEqual(pageUrl, "http://localhost:3000/");
+
+    // Test case 3: Incorrect password
+    await driver.get("http://localhost:3000/auth");
+    // Select input elements and fill them out
+    await driver.findElement(By.name("email")).sendKeys("test3@gmail.com");
+    await driver.findElement(By.name("password")).sendKeys("Password@1234");
+    await driver.findElement(By.className("ui button green")).click(); 
+    await driver.sleep(2000);
+    var pageUrl = await driver.getCurrentUrl();
+    // the user should stay on the login page
+    assert.strictEqual(pageUrl, "http://localhost:3000/auth");
+
+    // Test case 4: Trying to register an account with the same email
+    await driver.get("http://localhost:3000/auth");
+    await driver.findElement(By.linkText("Register")).click();
+    // Select input elements and fill them out
+    await driver.findElement(By.name("email")).sendKeys("test3@gmail.com");
+    await driver.findElement(By.name("password")).sendKeys("Password@12345");
+    await driver.findElement(By.className("ui button green")).click(); 
+    await driver.sleep(2000);
+    var pageUrl = await driver.getCurrentUrl();
+    assert.strictEqual(pageUrl, "http://localhost:3000/auth");
+
+    // Test case 5: Skip button
+    await driver.get("http://localhost:3000/auth");
+    await driver.findElement(By.className("ui right floated button")).click(); 
     await driver.sleep(2000);
     var pageUrl = await driver.getCurrentUrl();
     assert.strictEqual(pageUrl, "http://localhost:3000/");
