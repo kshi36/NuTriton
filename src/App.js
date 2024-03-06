b494b3d1d05e847cdf7b207011642593a10266b6
Dongyu-c <60305115+Dongyu-c@users.noreply.github.com>
2024-03-06 15:38:36 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Header from './views/header';
 import Navbar from './views/navbar';
 import RestaurantList from './views/restaurantlist';
 
+import Auth from './views/auth';
+import Profile from './views/profile';
+import Profile_setting from './views/profile_setting'
+import Edit from './views/edit profile'
+import Guest from './views/guest profile';
+// import Login from './views/login';
+// import Signup from './views/signup';
+
 import { ContextProvider } from "./controllers/restaurantcontext";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,7 +21,17 @@ function App() {
             <Header/>
             <ContextProvider>
                 <Routes>
-                    <Route exact path="/" element={<RestaurantList/>}/>
+                    {/* <Route exact path="/" element={<RestaurantList/>}/> */}
+                    <Route path="/auth" element={<Auth/>}/>
+                    {/*<Route path="/login" element={<Login/>}/>*/}
+                    {/*<Route path="/signup" element={<Signup/>}/>*/}
+                    <Route path= "/profile" element = {<Profile/>}/>
+                    <Route path= "/profile_setting" element = {<Profile_setting/>}/>
+                    <Route path= "/edit" element = {<Edit/>}/>
+                    <Route path= "/guest" element = {<Guest/>}/>
+                    
+
+                    
                 </Routes>
             </ContextProvider>
             <Navbar/>
