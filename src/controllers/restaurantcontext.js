9f1dc6fdc2ed76db4d986e5b8ce7d44ef215c821
Kevin <k1shi@ucsd.edu>
2024-03-06 12:46:44 -0800

    testing retrieval of food data per restaurant

--- src/controllers/restaurantcontext.js
+++ src/controllers/restaurantcontext.js
@@ -21,18 +21,34 @@ export function ContextProvider({ children }) {
     async function getRestaurants() {
         const res = await getDocs(collection(db, "restaurants"));
         if (res) {
-            //console logging
+            //restaurant data console logging
             // res.forEach((doc) => {
             //     // doc.data() is never undefined for query doc snapshots
             //     console.log(doc.id, " => ", doc.data());
             // });
 
-            setRestaurants(res.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+            const fulldocs = [];
+            for (const doc of res.docs) {
+                //TODO: retrieve foods list from Firestore
+                //retrieve food menu from every restaurant
+                const foodRes = await getDocs(collection(db, "restaurants", doc.id, "menu"));
+
+                //food data console logging
+                // foodRes.forEach((foodDoc) => {
+                //     // doc.data() is never undefined for query doc snapshots
+                //     console.log(foodDoc.id, " => ", foodDoc.data());
+                // });
+
+                const foods = foodRes.docs.map((foodDoc) => ({ id: foodDoc.id, ...foodDoc.data() }));
+
+                fulldocs.push({id: doc.id, foods, ...doc.data()});
+
+                setRestaurants(fulldocs);
+            }
+            // console.log("fulldocs:",fulldocs);
+            // await setRestaurants(fulldocs);
         }
     }
-
-    //TODO: retrieve foods list from Firestore
-
 
     //TODO: handle searching for restaurants & foods
     function searchHandler(searchTerm) {
--- src/views/restaurantlist.js
+++ src/views/restaurantlist.js
@@ -76,8 +76,9 @@ export default function RestaurantList() {
     useEffect(() => {
         getRestaurants();
 
+        console.log('incoming restaurant data:', restaurants.length);
         //logging
-        // restaurants.forEach((restaurant) => {console.log("restaurant data: ", restaurant)});
+        restaurants.forEach((restaurant) => {console.log("restaurant data: ", restaurant)});
     }, []);
 
     // const dummy_restaurants = ["Dirty Birds", "Margherita Pizza", "Tapioca Express", "Taco Villa", "Croutons", "64 degrees", "AI Dente", "Bird Rock", "Blue Bowl"];
