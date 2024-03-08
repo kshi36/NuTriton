e568dfe4a1452b454725997a619081c30a92dd86
Diean233 <wdxiop@outlook.com>
2024-03-08 13:55:35 -0800

    food card & styling

--- src/views/foodcard.js
+++ src/views/foodcard.js
@@ -8,13 +8,27 @@ export default function FoodCard({ food }) {
             <div>{Price}</div>
             </div>
             <div className="content">
-                <div className="center-div" style={{fontSize:'14px'}}>Nutrition facts:</div>
-                <div className="card">
+                <div className="center-div" style={{fontSize:'14px'}}>Nutrition facts:&nbsp;</div>
+                <div className="card nutrition">
                 {food['Vegan'] === "True" && (<div>Vegan;&nbsp;</div>)}
+                {food['Vegetarian'] === "True" && (<div>Vegetarian;&nbsp;</div>)}
+                {food['Sustainability'] === "True" && (<div>Sustainability;&nbsp;</div>)}
+                {food['Wellness'] === "True" && (<div>Wellness;&nbsp;</div>)}
 
                 </div>
-                <div className="card">
-                {food["Contains Soy"] === "True" && (<div>Contains Soy;&nbsp;</div>)}
+                <div className="card nutrition">
+                Contains:&nbsp;
+                {food["Contains Soy"] === "True" && (<div>Soy;&nbsp;</div>)}
+                {food["Contains Diary"] === "True" && (<div>Diary;&nbsp;</div>)}
+                {food["Contains Eggs"] === "True" && (<div>Eggs;&nbsp;</div>)}
+                {food["Contains Fish"] === "True" && (<div>Fish;&nbsp;</div>)}
+                {food["Contains Gluten"] === "True" && (<div>Gluten;&nbsp;</div>)}
+                {food["Contains Peanuts"] === "True" && (<div>Peanuts;&nbsp;</div>)}
+                {food["Contains Sesame"] === "True" && (<div>Sesame;&nbsp;</div>)}
+                {food["Contains Shell Fish"] === "True" && (<div>Shellfish;&nbsp;</div>)}
+                {food["Contains Tree Nuts"] === "True" && (<div>Tree nuts;&nbsp;</div>)}
+                {food["Contains Wheat"] === "True" && (<div>Wheat;&nbsp;</div>)}
+
 
                 </div>
             </div>
