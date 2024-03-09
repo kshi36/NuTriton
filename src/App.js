8c8a412e79f3b32bc9e7e9575b1eb9c0fd200891
Dongyu-c <60305115+Dongyu-c@users.noreply.github.com>
2024-03-08 16:53:10 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
-import Header from './views/header';
-import Navbar from './views/navbar';
-import RestaurantList from './views/restaurantlist';
+import Header from './views/header.js';
+import Navbar from './views/navbar.js';
+import RestaurantList from './views/restaurantlist.js';
+import Security_paper from "./views/security.js"
 
-import Auth from './views/auth';
-import Profile from './views/profile';
-import Profile_setting from './views/profile_setting'
-import Edit from './views/edit profile'
-import Guest from './views/guest profile';
+import Auth from './views/auth.js';
+import Profile from './views/profile.js';
+import Profile_setting from './views/profile_setting.js'
+import Edit from './views/edit profile.js'
+import Guest from './views/guest profile.js';
+import Map from './views/map.js'
 // import Login from './views/login';
 // import Signup from './views/signup';
 
-import { ContextProvider } from "./controllers/restaurantcontext";
+import { ContextProvider } from "./controllers/restaurantcontext.js";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoieGlvbmdkIiwiYSI6ImNsdGozcW9hejBsaDgyaXA1djE4YzcyZXkifQ.z-6FL9gKcIZvQJt5FPD4CA';
+
 
 function App() {
   return (
@@ -29,6 +36,9 @@ function App() {
                     <Route path= "/profile_setting" element = {<Profile_setting/>}/>
                     <Route path= "/edit" element = {<Edit/>}/>
                     <Route path= "/guest" element = {<Guest/>}/>
+                    <Route path= "/security_paper" element = {<Security_paper/>}/>
+                    <Route path= "/map" element = {<Map/>}/>
+                    
                     
 
                     
